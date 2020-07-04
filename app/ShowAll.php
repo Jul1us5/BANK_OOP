a4eaf398c25f1c70db57444d8517418966937fe4
Julius <Julius>
2020-07-05 02:13:25 +0300

    BANK OOP Step 8. | Show all

---
+++ app/ShowAll.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class ShowAll
+{
+
+    public static function showAll()
+    {
+        $data = file_get_contents('./../db/data.json');
+        $json_arr = json_decode($data, true);
+        $count = 1;
+        foreach ($json_arr as $index => $value) {
+            echo '<b>' . $count . '</b> ' . $value['name'] . ' ' . $value['surname'] . ' ' . $value['key'] . ' ' . $value['bill'] . ' ' . 'EUR.<br>';
+            $count++;
+        }
+    }
+}
