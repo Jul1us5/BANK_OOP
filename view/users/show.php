3ee1ce0f813fb94ef6b9b0c0511c599997fed651
Julius <Julius>
2020-07-11 23:11:58 +0300

    BANK OOP | method add or remove money

--- view/users/show.php
+++ view/users/show.php
@@ -3,28 +3,55 @@
 namespace App;
 
 use App\DB\JsonDb as DB;
+use App\App;
+use App\DB\JsonDb;
 
-$data = DB::json();
+$data= DB::json();
 
 
 if (self::$params[2] == 'show') {
+   
+
+
+
+
+
+
+
+
+  
     foreach($data as $index => $value){
+        
+
+        
         echo '<form action="show" method="post">';
         echo '<input type="text" name="id" value="'.$index.'">';
         echo '<button type="submit">Rodyti</button>';
         echo '</form>';
+      
+
     }
-} 
+}
 if(isset($_POST['id'])) {
+    $_SESSION['id'] = $_POST['id'];
     $data = DB::show($_POST['id']);
     foreach($data as $key => $value) {
         if($key == 'pass' ) continue;
             echo $value . '<br/>';
     }
-    
-    echo $_POST['id'] . '<-- INDEX<br/>';
+    // echo $_POST['id'] . '<-- INDEX<br/>';
+    // ...
     echo '<form action="delete" method="post">';
     echo '<input type="hidden" id="id" name="id" value="' . $_POST['id']. '">';
     echo '<button type="submit" name="delete" value="delete">Delete</button>';
     echo '</form>';
+    echo '<form action="" method="post">';
+    echo '<input name="name" value="">';
+    echo '<button type="submit" name="plus" value="plus">+</button>';
+    
+
+  
+    echo '<button type="submit" name="minus" value="minus">-</button>';
+    echo '</form>';
 }
+echo App::Message();
